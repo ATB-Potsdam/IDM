743faf259ccebc74d8b191d7179c7088744e6e79
uhucrew <uwe@runlevel3.de>
2015-09-03 16:33:55 +0200

    no change

--- ATB_Irrigation-Module_cs/version.cs
+++ ATB_Irrigation-Module_cs/version.cs
@@ -25,8 +25,8 @@ namespace version
     {
         public const String VERSION_MAJOR = "1";
         public const String VERSION_MINOR = "0";
-        public const String VERSION_REVISION = "10";
-        public const String VERSION_BUILD = "388";
+        public const String VERSION_REVISION = "11";
+        public const String VERSION_BUILD = "389";
         public const String VER_FILE_VERSION_STR = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_REVISION + "." + VERSION_BUILD;
 
 #if _WIN64
